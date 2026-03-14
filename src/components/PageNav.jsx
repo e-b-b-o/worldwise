0cd00a1de445dee51e64008e6b7c1f15599a9fb2
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-03-14 23:49:48 +0300

    added new components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,22 +1,24 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "../components/Logo";
 
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
-      <Logo />
+      <NavLink to="/">
+        <Logo />
+      </NavLink>
       <ul>
         <li>
-          <Link to="/pricing">Pricing</Link>
+          <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <Link to="/product">Product</Link>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <Link to="/login" className={styles.ctaLink}>
+          <NavLink to="/login" className={styles.ctaLink}>
             Login
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
