14707810a654728a3aa5ffbcf19e04ad8c451d10
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-03-15 23:24:18 +0300

    added context api into worldwise

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,10 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContexts";
 
-export default function CityList({ cities, isLoading }) {
+export default function CityList() {
+  const { isLoading, cities } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
